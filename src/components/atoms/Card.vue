7b960dcea70e2381443e4fb53308e71a142552a6
Talita-8 <talita.silva@pravaler.com>
2021-10-03 15:52:20 -0300

    feat: Disposição dos cards na tela

--- src/components/atoms/Card.vue
+++ src/components/atoms/Card.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-      <img :src="info[0].image"/>
-        <p>
+    <div class="card">
+      <img :src="info.image" class="image"/>
+        <p class="infos">
             <ul>
-                <li>{{info[0].name}}</li>
-                <li>{{info[0].status}}</li>
-                <li>{{info[0].species}}</li>
-                <li>{{info[0].gender}}</li>
+                <li>Name: {{info.name}}</li>
+                <li>Status: {{info.status}}</li>
+                <li>Specie: {{info.species}}</li>
+                <li>Gender: {{info.gender}}</li>
             </ul>
         </p>
     </div>
@@ -22,6 +22,35 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.3rem;
+    background-color: rgb(20, 20, 20);
+    border-radius: 0.5rem;
+    width: fit-content;
+    margin: 1rem;
+  }
+  .image {
+   border-radius: 1rem;
+   width: 10rem;
+   height: 10rem;
+   border: 0.2rem solid rgb(2, 255, 234);
+ }
+ ul {
+   padding-left: 0;
+ }
+ li {
+   list-style: none;
+ }
+ .infos {
+   padding: 1rem;
+   background-color: black;
+   color: chartreuse;
+   border-radius: 0 0.2rem 0.2rem 0;
+   font-family: 'Quantico', sans-serif;
+ }
 </style>
+ 
